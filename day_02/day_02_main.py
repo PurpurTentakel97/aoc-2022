f8470210d9e43c7dc6ab5c708a4ac832b445c2d3
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-12-05 22:50:37 +0100

    day 2.1

--- day_02/day_02_main.py
+++ day_02/day_02_main.py
@@ -12,7 +12,13 @@ class TicTacToeType(IntEnum):
     SCISSORS = 3
 
 
-def get_enum_from_string(value: str) -> TicTacToeType:
+class Result(IntEnum):
+    LOOSE = 0
+    DRAW = 3
+    WIN = 6
+
+
+def get_tic_tac_toe_from_string(value: str) -> TicTacToeType:
     match value:
         case "A":
             return TicTacToeType.ROCK
@@ -29,32 +35,82 @@ def get_enum_from_string(value: str) -> TicTacToeType:
             return TicTacToeType.SCISSORS
 
 
+def get_result_from_string(value: str) -> Result:
+    match value:
+        case "X":
+            return Result.LOOSE
+        case "Y":
+            return Result.DRAW
+        case "Z":
+            return Result.WIN
+
+
+def get_score1(enemy, me) -> int:
+    score: int = 0
+
+    score += me
+
+    if enemy == me:
+        score += Result.DRAW
+    else:
+        match enemy:
+            case TicTacToeType.ROCK:
+                if me == TicTacToeType.PAPER:
+                    score += Result.WIN
+            case TicTacToeType.PAPER:
+                if me == TicTacToeType.SCISSORS:
+                    score += Result.WIN
+            case TicTacToeType.SCISSORS:
+                if me == TicTacToeType.ROCK:
+                    score += Result.WIN
+
+    return score
+
+
+def get_score2(enemy, result) -> int:
+    score: int = 0
+
+    score += result
+
+    match result:
+        case Result.WIN:
+            match enemy:
+                case TicTacToeType.ROCK:
+                    score += TicTacToeType.PAPER
+                case TicTacToeType.PAPER:
+                    score += TicTacToeType.SCISSORS
+                case TicTacToeType.SCISSORS:
+                    score += TicTacToeType.ROCK
+        case Result.DRAW:
+            score += enemy
+        case Result.LOOSE:
+            match enemy:
+                case TicTacToeType.ROCK:
+                    score += TicTacToeType.SCISSORS
+                case TicTacToeType.PAPER:
+                    score += TicTacToeType.ROCK
+                case TicTacToeType.SCISSORS:
+                    score += TicTacToeType.PAPER
+
+    return score
+
+
 def d_02_main() -> None:
-    with open("day_02/input_02_1.txt", "r") as file:
-        score: int = 0
+    with open("day_02/input_02_2.txt", "r") as file:
+        score1: int = 0
+        score2: int = 0
+
         for line in file:
             if len(line) == 0:
                 continue
 
-            enemy, me = line.strip().split(" ")
-            enemy = get_enum_from_string(enemy)
-            me = get_enum_from_string(me)
+            enemy, second = line.strip().split(" ")
+            enemy = get_tic_tac_toe_from_string(enemy)
+            predicted_me = get_tic_tac_toe_from_string(second)
+            predicted_result = get_result_from_string(second)
 
-            score += me
+            score1 += get_score1(enemy, predicted_me)
+            score2 += get_score2(enemy, predicted_result)
 
-            if enemy == me:
-                score += 3
-                continue
-
-            match enemy:
-                case TicTacToeType.ROCK:
-                    if me == TicTacToeType.PAPER:
-                        score += 6
-                case TicTacToeType.PAPER:
-                    if me == TicTacToeType.SCISSORS:
-                        score += 6
-                case TicTacToeType.SCISSORS:
-                    if me == TicTacToeType.ROCK:
-                        score += 6
-
-        print("the total score is: " + str(score))
+        print("the total score1 is: " + str(score1))
+        print("the total score2 is: " + str(score2))
