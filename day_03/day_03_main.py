4eeddf5367074bb58c630210a44cbae169c8c78b
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-12-05 19:26:08 +0100

    day 3

--- day_03/day_03_main.py
+++ day_03/day_03_main.py
@@ -4,4 +4,20 @@
 #
 
 def d_03_main() -> None:
-    pass
+    with open("day_03/real_input_03.txt", "r") as file:
+        score: int = 0
+        for line in file:
+            first_part, second_part = line[:len(line) // 2], line[len(line) // 2:]
+
+            for char in first_part:
+                if char in second_part:
+                    character: str = char
+                    character_int: int = ord(character)
+                    if character.islower():
+                        character_int -= 96
+                    else:
+                        character_int -= 38
+                    score += character_int
+                    break
+
+        print("the score is: " + str(score))
