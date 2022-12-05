6c0796fe2b99781414d340f9966f3e28d6a79673
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-12-05 23:03:38 +0100

    day 3.1

--- day_03/day_03_main.py
+++ day_03/day_03_main.py
@@ -3,21 +3,35 @@
 # 05.12.2022
 #
 
+def get_number_from_char(char: str) -> int:
+    character_int: int = ord(char)
+    if char.islower():
+        character_int -= 96
+    else:
+        character_int -= 38
+
+    return character_int
+
+
 def d_03_main() -> None:
-    with open("day_03/input_03_1.txt", "r") as file:
-        score: int = 0
-        for line in file:
+    with open("day_03/input_03_2.txt", "r") as file:
+        score1: int = 0
+        score2: int = 0
+
+        lines: list[str] = file.readlines()
+        for i, line in enumerate(lines):
             first_part, second_part = line[:len(line) // 2], line[len(line) // 2:]
 
             for char in first_part:
                 if char in second_part:
-                    character: str = char
-                    character_int: int = ord(character)
-                    if character.islower():
-                        character_int -= 96
-                    else:
-                        character_int -= 38
-                    score += character_int
+                    score1 += get_number_from_char(char)
                     break
 
-        print("the score is: " + str(score))
+            if i % 3 == 0:
+                for char in lines[i]:
+                    if char in lines[i+1] and char in lines[i+2]:
+                        score2 += get_number_from_char(char)
+                        break
+
+        print("the score1 is: " + str(score1))
+        print("the score2 is: " + str(score2))
