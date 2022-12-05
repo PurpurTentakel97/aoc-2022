3c1a9155540c1ee69325d3def2d0c38eb30965be
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-12-05 19:01:59 +0100

    day 2

--- day_02/day_02_main.py
+++ day_02/day_02_main.py
@@ -3,5 +3,58 @@
 # 05.12.2022
 #
 
+from enum import IntEnum
+
+
+class TicTacToeType(IntEnum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+def get_enum_from_string(value: str) -> TicTacToeType:
+    match value:
+        case "A":
+            return TicTacToeType.ROCK
+        case "B":
+            return TicTacToeType.PAPER
+        case "C":
+            return TicTacToeType.SCISSORS
+
+        case "X":
+            return TicTacToeType.ROCK
+        case "Y":
+            return TicTacToeType.PAPER
+        case "Z":
+            return TicTacToeType.SCISSORS
+
+
 def d_02_main() -> None:
-    pass
+    with open("day_02/real_input_02.txt", "r") as file:
+        score: int = 0
+        for line in file:
+            if len(line) == 0:
+                continue
+
+            enemy, me = line.strip().split(" ")
+            enemy = get_enum_from_string(enemy)
+            me = get_enum_from_string(me)
+
+            score += me
+
+            if enemy == me:
+                score += 3
+                continue
+
+            match enemy:
+                case TicTacToeType.ROCK:
+                    if me == TicTacToeType.PAPER:
+                        score += 6
+                case TicTacToeType.PAPER:
+                    if me == TicTacToeType.SCISSORS:
+                        score += 6
+                case TicTacToeType.SCISSORS:
+                    if me == TicTacToeType.ROCK:
+                        score += 6
+
+        print("the total score is: " + str(score))
