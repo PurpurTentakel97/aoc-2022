10a435d7e03c01555bf9ad50610786145eb2f041
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-12-16 21:05:53 +0100

    add titles to days

--- main.py
+++ main.py
@@ -21,46 +21,46 @@ if __name__ == "__main__":
 
         match day:
             case 1:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Calorie Counting\"\n")
                 d.d_01.d_01_main()
             case 2:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Rock Paper Scissors\"\n")
                 d.d_02.d_02_main()
             case 3:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Rucksack Reorganization\"\n")
                 d.d_03.d_03_main()
             case 4:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Camp Cleanup\"\n")
                 d.d_04.d_04_main()
             case 5:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Supply Stacks\"\n")
                 d.d_05.d_05_main()
             case 6:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Tuning Trouble\"\n")
                 d.d_06.d_06_main()
             case 7:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"No Space Left On Device\"\n")
                 d.d_07.d_07_main()
             case 8:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Treetop Tree House\"\n")
                 d.d_08.d_08_main()
             case 9:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Rope Bridge\"\n")
                 d.d_09.d_09_main()
             case 10:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Cathode-Ray Tube\"\n")
                 d.d_10.d_10_main()
             case 11:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Monkey in the Middle\"\n")
                 d.d_11.d_11_main()
             case 12:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Hill Climbing Algorithm\"\n")
                 d.d_12.d_12_main()
             case 13:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Distress Signal\"\n")
                 d.d_13.d_13_main()
             case 14:
-                print("\nday:" + str(day) + "\n")
+                print("\nday:" + str(day) + " \"Regolith Reservoir\"\n")
                 d.d_14.d_14_main()
             case 15:
                 print("\nday:" + str(day) + "\n")
